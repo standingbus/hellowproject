bbe04132b3e2cca5d197e137b0220fc30394d56e
admin <admin@DESKTOP-7PDSB48>
2023-05-30 10:40:57 +0900

    first commit.

---
+++ HelloApp/src/com/yedam/HelloApp.java
@@ -0,0 +1,13 @@
+package com.yedam;
+
+public class HelloApp {
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 50;
+		int num3 = 10;// 추가.
+		int result = num1 + num2;
+		result = result - num3;
+		System.out.println("결과값:" + result);
+		
+	}//end of main.
+}// end of class.
---
+++ HelloApp/src/com/yedam/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.yedam;
+
+public class HelloWorld {
+	// main 메소드 시작.
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		System.out.println("git 연동.");
+	}
+}
+//컴파일을 해줘야해요 콜록콜록 저장만하면 이클립스가 클래스 저장해줘요 
